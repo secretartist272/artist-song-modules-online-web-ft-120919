e34482f1215721c9cbb8c8f8de1cd6a783a8665a
Jacqueline Freeland <secretartist272@gmail.com>
2020-01-08 02:19:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,14 @@
 require 'pry'
 
+
 class Artist
-  extend Memorable::Count 
-  extend Memorable::Reset_all
-  
   attr_accessor :name
   attr_reader :songs
+
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Memorable::InstanceMethods
+  include Paramable::InstanceMethods
 
   @@artists = []
 
@@ -22,24 +25,13 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def add_song(song)
     @songs << song
-    song.artist = self
+    song.artist = self unless song.artist
   end
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
 end
